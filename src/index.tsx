1106ca67531af6e292e1b2f7ccce9c4d3edeca94
Frank J. Reashore <reashore@telus.net>
2019-03-04 18:38:22 -0800

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+type RootType =
+  | React.FunctionComponentElement<any>
+  | React.FunctionComponentElement<any>[];
+const appRoot: RootType = <App />;
+const root: Element | null = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(appRoot, root);
